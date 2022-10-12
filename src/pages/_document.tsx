69f0da6cd8be35b17b5694a062067538b4dad5ee
aldojunior990 <aldojunior990@gmail.com>
2022-10-12 11:02:54 -0300

    readme

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -43,6 +43,7 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
             rel="stylesheet"
           />
+          <title>Meu blog | AldoJr</title>
         </Head>
 
         <body>
